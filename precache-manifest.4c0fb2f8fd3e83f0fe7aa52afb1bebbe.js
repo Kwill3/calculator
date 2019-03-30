4f24ef7d117fbce3ed2e00bc5c98f59114506974
William Lee <karlweiyang@gmail,com>
2019-03-30 22:46:41 +0000

    Updates

---
+++ precache-manifest.4c0fb2f8fd3e83f0fe7aa52afb1bebbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a27725b30becb85a1332be9a88ba51e",
+    "url": "/calculator/static/media/backspace.8a27725b.svg"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "200f33668245cc3445d1",
+    "url": "/calculator/static/js/main.96cda6b1.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/calculator/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "200f33668245cc3445d1",
+    "url": "/calculator/static/css/main.e61a107f.chunk.css"
+  },
+  {
+    "revision": "8615cf45d379a1810962da880d588b0c",
+    "url": "/calculator/index.html"
+  }
+];
