0bb1947c71e99df037077a1c5ebb06b00ba62082
William Lee <karlweiyang@gmail,com>
2019-03-30 22:48:19 +0000

    deploy app to GitHub pages

--- src/App.js
+++ src/App.js
@@ -11,15 +11,24 @@ class App extends Component {
       input: "",
       curr: "",
       prev: "",
-      operator: ""
+      operator: "",
+      pressed: false
     }
   }
 
   addToInput = val => {
     // Appends a number to the input
-    this.setState({
-      input: this.state.input + val
-    })
+    if (this.state.pressed === true){
+      // Resets the calculator after completing a calculation
+      this.clearAll();
+      this.setState({
+        input: val
+      })
+    } else {
+      this.setState({
+        input: this.state.input + val
+      })
+    }
   }
 
   addZero = val => {
@@ -42,71 +51,242 @@ class App extends Component {
       input: "",
       curr: "",
       prev: "",
-      operator: ""
+      operator: "",
+      pressed: false
     })
   }
 
   clearOne = () => {
     // Clear the last number from input
-    this.setState({
-      input: this.state.input.substr(0, this.state.input.length - 1)
-    })
+    if (this.state.pressed !== true) {
+      this.setState({
+        input: this.state.input.substr(0, this.state.input.length - 1)
+      })
+    }
   }
 
   add = () => {
     // Addition operator
-    this.setState({
-      prev: this.state.input,
-      input: "",
-      operator: "+"
-    });
+    if (this.state.operator === "" && this.state.prev === "") {
+      // Prints prev number if no number exists
+      this.setState({
+        prev: this.state.input,
+        input: "",
+        operator: "+"
+      });
+    } 
+    else if (this.state.curr === "" && this.state.operator === "+"){
+      // Adds number to prev if user presses '+' instead of '='
+      this.setState({
+        prev : (parseFloat(this.state.prev) + parseFloat (this.state.input)),
+        input: "",
+        operator: "+"
+      })
+    } 
+    else if (this.state.operator !== "+" && this.state.curr === "" && this.state.input !== ""){
+      // Calls the changeOperators function to complete the outstanding equation before changing to a new operator
+      this.changeOperators();
+      this.setState({
+        operator: "+",
+        input: ""
+      })
+    } 
+    else if (this.state.operator !== "+" && this.state.curr === ""){
+      // Changes operator to '+' if it is not already
+      this.setState({
+        operator: "+"
+      })
+    } 
+    else {
+      // Adds number to prev and resets the input and curr if the user used '=' previously
+      this.setState({
+        prev : this.state.input,
+        curr: "",
+        input: "",
+        operator: "+",
+        pressed: false
+      })
+    }
   }
 
   sub = () => {
     // Subtraction operator
-    this.setState({
-      prev: this.state.input,
-      input: "",
-      operator: "-"
-    });
+    if (this.state.operator === "" && this.state.prev === "") {
+      // Prints prev number if no number exists
+      this.setState({
+        prev: this.state.input,
+        input: "",
+        operator: "-"
+      });
+    } 
+    else if (this.state.curr === "" && this.state.operator === "-"){
+      // Subtracts number to prev if user presses '-' instead of '='
+      this.setState({
+        prev : (parseFloat(this.state.prev) - parseFloat (this.state.input)),
+        input: "",
+        operator: "-"
+      })
+    } 
+    else if (this.state.operator !== "-" && this.state.curr === "" && this.state.input !== ""){
+      // Calls the changeOperators function to complete the outstanding equation before changing to a new operator
+      this.changeOperators();
+      this.setState({
+        operator: "-",
+        input: ""
+      })
+    } 
+    else if (this.state.operator !== "-" && this.state.curr === ""){
+      // Changes operator to '-' if it is not already
+      this.setState({
+        operator: "-"
+      })
+    } 
+    else {
+      // Subtracts number to prev and resets the input and curr if the user used '=' previously
+      this.setState({
+        prev : this.state.input,
+        curr: "",
+        input: "",
+        operator: "-",
+        pressed: false
+      })
+    }
   }
 
   mul = () => {
     // Multiplication operator
-    this.setState({
-      prev: this.state.input,
-      input: "",
-      operator: "*"
-    });
+    if (this.state.operator === "" && this.state.prev === "") {
+      // Prints prev number if no number exists
+      this.setState({
+        prev: this.state.input,
+        input: "",
+        operator: "*"
+      });
+    } 
+    else if (this.state.curr === "" && this.state.operator === "*"){
+      // Multiplies number to prev if user presses '*' instead of '='
+      this.setState({
+        prev : (parseFloat(this.state.prev) * parseFloat (this.state.input)),
+        input: "",
+        operator: "*"
+      })
+    } 
+    else if (this.state.operator !== "*" && this.state.curr === "" && this.state.input !== ""){
+      // Calls the changeOperators function to complete the outstanding equation before changing to a new operator
+      this.changeOperators();
+      this.setState({
+        operator: "*",
+        input: ""
+      })
+    } 
+    else if (this.state.operator !== "*" && this.state.curr === ""){
+      // Changes operator to '*' if it is not already
+      this.setState({
+        operator: "*"
+      })
+    } 
+    else {
+      // Multiplies number to prev and resets the input and curr if the user used '=' previously
+      this.setState({
+        prev : this.state.input,
+        curr: "",
+        input: "",
+        operator: "*",
+        pressed: false
+      })
+    }
   }
 
   div = () => {
     // Division operator
-    this.setState({
-      prev: this.state.input,
-      input: "",
-      operator: "/"
-    });
+    if (this.state.operator === "" && this.state.prev === "") {
+      // Prints prev number if no number exists
+      this.setState({
+        prev: this.state.input,
+        input: "",
+        operator: "/"
+      });
+    } 
+    else if (this.state.curr === "" && this.state.operator === "/"){
+      // Divides number to prev if user presses '/' instead of '='
+      this.setState({
+        prev : (parseFloat(this.state.prev) / parseFloat (this.state.input)),
+        input: "",
+        operator: "/"
+      })
+    } 
+    else if (this.state.operator !== "/" && this.state.curr === "" && this.state.input !== ""){
+      // Calls the changeOperators function to complete the outstanding equation before changing to a new operator
+      this.changeOperators();
+      this.setState({
+        operator: "/",
+        input: ""
+      })
+    } 
+    else if (this.state.operator !== "/" && this.state.curr === ""){
+      // Changes operator to '/' if it is not already
+      this.setState({
+        operator: "/"
+      })
+    } 
+    else {
+      // Divides number to prev and resets the input and curr if the user used '=' previously
+      this.setState({
+        prev : this.state.input,
+        curr: "",
+        input: "",
+        operator: "/",
+        pressed: false
+      })
+    }
   }
 
+  // Function that finishes evaluating the previous calculation before moving on to change into another operator
+  changeOperators = () => {
+    if (this.state.operator === "+") {
+      this.setState({
+        prev : (parseFloat(this.state.prev) + parseFloat (this.state.input))
+      })
+    } else if (this.state.operator === "-") {
+      this.setState({
+        prev : (parseFloat(this.state.prev) - parseFloat (this.state.input))
+      })
+    } else if (this.state.operator === "/") {
+      this.setState({
+        prev : (parseFloat(this.state.prev) / parseFloat (this.state.input))
+      })
+    } else if (this.state.operator === "*") {
+      this.setState({
+        prev : (parseFloat(this.state.prev) * parseFloat (this.state.input))
+      })
+    }
+  }
+
+  // Evaluate the equation according to operators used
   evaluate = () => {
-    // Evaluate the equation according to operators used
-    this.setState({curr: this.state.input}, ()=> {
-      if (this.state.operator === "+") {
+    // Change pressed boolean to reset calculator after calculation
+    this.setState({curr: this.state.input, pressed: true}, ()=> {
+
+      if (this.state.operator === "+" && this.state.curr !== "") {
         this.setState({
           input : (parseFloat(this.state.prev) + parseFloat (this.state.curr))
         })
-      } else if (this.state.operator === "-") {
+      } else if (this.state.operator === "-" && this.state.curr !== "") {
         this.setState({
           input : (parseFloat(this.state.prev) - parseFloat (this.state.curr))
         })
-      } else if (this.state.operator === "/") {
+      } else if (this.state.operator === "/" && this.state.curr !== "") {
         this.setState({
           input : (parseFloat(this.state.prev) / parseFloat (this.state.curr))
         })
-      } else if (this.state.operator === "*") {
+      } else if (this.state.operator === "*" && this.state.curr !== "") {
         this.setState({
           input : (parseFloat(this.state.prev) * parseFloat (this.state.curr))
+        })
+      } else {
+        // Do nothing if curr value is blank (reset the pressed boolean to false)
+        this.setState({
+          pressed: false
         })
       }
     })
@@ -117,20 +297,22 @@ class App extends Component {
     this.setState({curr: this.state.input}, ()=> {
       if (this.state.operator === "+") {
         this.setState({
-          input : (parseFloat(this.state.prev) + parseFloat (this.state.prev*(this.state.curr/100)))
+          input : (parseFloat(this.state.prev) + parseFloat(this.state.prev*(this.state.curr/100)))
         })
       } else if (this.state.operator === "-") {
         this.setState({
-          input : (parseFloat(this.state.prev) - parseFloat (this.state.prev*(this.state.curr/100)))
+          input : (parseFloat(this.state.prev) - parseFloat(this.state.prev*(this.state.curr/100)))
         })
       } else if (this.state.operator === "/") {
         this.setState({
-          input : (parseFloat(this.state.prev) / parseFloat (this.state.prev*(this.state.curr/100)))
+          input : (parseFloat(this.state.prev) / parseFloat(this.state.curr/100))
         })
       } else if (this.state.operator === "*") {
         this.setState({
-          input : (parseFloat(this.state.prev) * parseFloat (this.state.prev*(this.state.curr/100)))
+          input : (parseFloat(this.state.prev) * parseFloat(this.state.curr/100))
         })
+      } else {
+        return;
       }
     })
   }
@@ -147,7 +329,7 @@ class App extends Component {
           </div>
           <div className="row">
             <Button handleClick={this.clearAll}>AC</Button>
-            <Button handleClick={this.clearOne}><img src={backspace} className='backspace-icon' alt="icon"/></Button>
+            <Button handleClick={() => this.clearOne()}><img src={backspace} className='backspace-icon' alt="icon"/></Button>
             <Button handleClick={() => this.percentage()}>%</Button>
             <Button handleClick={this.div}>/</Button>
           </div>
